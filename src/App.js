865ff9c4b5637779d649ebf5da97ceefe834cd12
grabfast <grabfast001@gmail.com>
2021-07-25 00:46:41 +0530

    Domain configure gh pages

--- src/App.js
+++ src/App.js
@@ -48,7 +48,8 @@ class App extends React.Component{
       <div>
         <Header />
         <Switch>
-          <Route exact path="/ReactJS-ecommerce-template" component={HomePage} />
+          {/* <Route exact path="/" component={HomePage} />  */}
+          <Route exact path="/GrabFast" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
           <Route exact path='/checkout' component={CheckoutPage} />
           <Route 
